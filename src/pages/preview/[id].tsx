9e4a3c937624d4c1f80c587792b2c9df2a3bf0c1
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2023-01-15 13:27:22 +0700

    feat: integration preview parse html

--- src/pages/preview/[id].tsx
+++ src/pages/preview/[id].tsx
@@ -1,10 +1,47 @@
 import { useRouter } from "next/router"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import db from "@/firebase/firebaseInit"
+import parse from "html-react-parser"
+import MetaData from "@/components/elements/MetaData"
+import { GetServerSideProps, GetServerSidePropsContext } from "next"
 
-const PreviewByIdPage = () => {
+const PreviewByIdPage = ({ block }) => {
   const router = useRouter()
   const { id } = router.query
-  return <div>{id}</div>
+  const stringId = typeof id === "string" ? id : ""
+
+  if (!block) return <p>Loading..</p>
+  return (
+    <>
+      <MetaData title="Preview Blocks" />
+      <div>{parse(block)}</div>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const {
+    query: { id },
+  } = context
+  const stringId = typeof id === "string" ? id : ""
+
+  const blocks = db.collection("blocks").doc(stringId)
+  const res = await blocks.get()
+  if (res.exists) {
+    const { content } = res.data()
+    return {
+      props: {
+        block: content,
+      },
+    }
+  }
+  return {
+    props: {
+      block: null,
+    },
+  }
 }
 
 export default PreviewByIdPage
