2d273a0c3e19c4682cc26667cfdeaa52a835d94a
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2023-01-16 15:27:20 +0700

    feat: import cdn tailwind in metadata

--- src/components/elements/MetaData.tsx
+++ src/components/elements/MetaData.tsx
@@ -4,15 +4,18 @@ import Head from "next/head"
 interface MetaDataProps {
   title: string
   description?: string
+  isUsingCDNTailwind?: boolean
 }
 
 const MetaData = (props: MetaDataProps) => {
-  const { title, description } = props
+  const { title, description, isUsingCDNTailwind = false } = props
   return (
     <Head>
       <title>{title} | CMS Content</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
+
+      {isUsingCDNTailwind && <script src="https://cdn.tailwindcss.com"></script>}
     </Head>
   )
 }
